66b63307a0076a6281bf5f78623bc5fd91deb312
richelbilderbeek <richel@richelbilderbeek.nl>
2020-09-05 18:26:00 +0200

    Use 'mcbette::interpret_marg_liks_estimates'

--- scripts/run.R
+++ scripts/run.R
@@ -17,51 +17,5 @@ if (can_run_mcbette()) {
   )
   inference_models <- list(inference_model_1, inference_model_2)
 
-  df <- mcbette::est_marg_liks(
-    fasta_filename = fasta_filename,
-    inference_models = inference_models
-  )
-
-  # Show all models
-  cat(" ", sep = "\n")
-  cat("**********************************", sep = "\n")
-  cat("* All models (ordered by index)  *", sep = "\n")
-  cat("**********************************", sep = "\n")
-  cat(" ", sep = "\n")
-  knitr::kable(df)
-
-  ################################################################################
-  # Create an ordered data frame
-  ################################################################################
-  # Keep rows without an NA
-  df_ordered <- na.omit(df)
-
-  # Order from high to low
-  df_ordered <- df_ordered[ order(-df_ordered$weight), ]
-
-  # Show most convincing first
-  cat(" ", sep = "\n")
-  cat("**********************************", sep = "\n")
-  cat("* All models (ordered by weight) *", sep = "\n")
-  cat("**********************************", sep = "\n")
-  cat(" ", sep = "\n")
-  knitr::kable(df_ordered)
-
-  # Show weights
-  cat(" ", sep = "\n")
-  txtplot::txtplot(x = seq(1, length(df_ordered$weight)), y = df_ordered$weight)
-
-  ################################################################################
-  # Create a best model data frame
-  ################################################################################
-  # Show the best model
-  best_row_index <- which(df_ordered$marg_log_lik == max(df_ordered$marg_log_lik))
-  df_best <- df_ordered[best_row_index, ]
-  cat(" ", sep = "\n")
-  cat("**********************************", sep = "\n")
-  cat("* Best model:                    *", sep = "\n")
-  cat("**********************************", sep = "\n")
-  cat(" ", sep = "\n")
-  knitr::kable(df_best, row.names = FALSE)
-
+  interpret_marg_lik_estimates(marg_liks)
 }
